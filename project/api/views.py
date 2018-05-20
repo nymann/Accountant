f5ab5a0f73415752d6b44aae720192abe9983c7a
Kristian Nymann Jakobsen <nymannjakobsen@gmail.com>
2018-05-20 20:54:15 +0200

    Added calendar, can't get background-color or color working, only border-color.

--- project/api/views.py
+++ project/api/views.py
@@ -1,6 +1,30 @@
+from sqlalchemy.sql import label
+
 from project.api import api
+from flask import request, jsonify
+from datetime import datetime, date
+from project.models import db, Dinner, User
 
 
 @api.route('/')
 def index():
     return "{results: bla}"
+
+
+@api.route('/dinners')
+def dinners():
+    start = request.args.get('start')
+    end = request.args.get('end')
+
+    start = datetime.strptime(start, "%Y-%m-%d") if start else datetime.now()
+    end = datetime.strptime(end, "%Y-%m-%d") if end else datetime.strptime("3000-01-01 23:59:59", "%Y-%m-%d %H:%M:%S")
+
+    dinners = db.session.query(
+        label("title", User.name),
+        label("url", Dinner.id),
+        label("start", Dinner.date)
+    ).filter(
+        Dinner.date >= start,
+        Dinner.date <= end
+    ).all()
+    return jsonify(dinners)
---
+++ project/static/js/calendar.js
@@ -0,0 +1,34 @@
+function calendarInit(defaultView) {
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            defaultView: defaultView,
+            right: 'month, basicWeek,basicDay'
+        },
+        defaultDate: new Date(),
+        allDay: true,
+        navLinks: true,
+        editable: false,
+        eventLimit: true,
+        events: {
+            url: '/api/dinners',
+            error: function (error) {
+                alert("Couldn't get data from URL " + error);
+            }
+        },
+        eventClick: function (event) {
+            if (event.url) {
+                window.open('/dinner_club/meal/' + event.url);
+                return false;
+            }
+        },
+        eventRender: function (event, element) {
+            element.css({
+               'background-color': '#ff1e00',
+                'border-color': '#ff1e00',
+                'color': '#ff1e00'
+            });
+        }
+    });
+}

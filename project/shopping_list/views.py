b96d9be0f5af67ce49cc0fdd0c354c16a4cfbdcd
Kristian Nymann Jakobsen <nymannjakobsen@gmail.com>
2018-05-20 16:48:11 +0200

    Closes #33 and Closes #34.

--- project/shopping_list/views.py
+++ project/shopping_list/views.py
@@ -1,4 +1,5 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, abort, request, redirect, url_for
+from flask_login import current_user
 from project.shopping_list import shopping_list
 from project.models import Shopping, User, db, Items
 from project.forms import ShoppingForm, ItemForm
@@ -12,6 +13,12 @@ def index():
         Shopping.accounted.is_(False)
     ).all()
     return render_template('shopping_list/index.html', shopping_list_entries=shopping_list_entries)
+
+
+@shopping_list.route('/<shopping_id>')
+def entry(shopping_id):
+    entry = Shopping.query.get_or_404(int(shopping_id))
+    return render_template('shopping_list/shopping_entry.html', entry=entry)
 
 
 @shopping_list.route('/new', methods=['GET', 'POST'])
@@ -38,8 +45,45 @@ def new():
     return render_template('shopping_list/new.html', users=users, form=form, today=today)
 
 
-@shopping_list.route('/items/new/<shopping_id>', methods=['GET', 'POST'])
-def items_new(shopping_id):
+@shopping_list.route('/edit/<shopping_id>', methods=['GET', 'POST'])
+def edit(shopping_id):
+    form = ShoppingForm()
+    shopping = Shopping.query.get_or_404(int(shopping_id))
+    if form.validate_on_submit():
+        try:
+            shopping.payee_id = form.payee.data if (
+                    current_user is shopping.payee or current_user.admin) else shopping.payee_id
+            shopping.date = datetime.strptime(form.date.data, "%d/%m/%Y")
+            db.session.commit()
+            flash("Successfully updated shopping entry.", "alert alert-info")
+        except (DBAPIError, ValueError) as e:
+            flash(str(e), "alert alert-danger")
+            db.session.rollback()
+    users = User.query.filter(
+        User.active
+    ).all()
+    return render_template('shopping_list/edit.html', shopping=shopping, users=users, form=form)
+
+
+@shopping_list.route('/delete/<shopping_id>')
+def delete(shopping_id):
+    shopping = Shopping.query.get_or_404(int(shopping_id))
+    if current_user is not shopping.payee and not current_user.admin:
+        return abort(502)
+
+    try:
+        db.session.delete(shopping)
+        db.session.commit()
+        flash("Shopping entry removed successfully", "alert alert-info")
+    except DBAPIError as e:
+        flash(str(e), "alert alert-danger")
+        db.session.rollback()
+    return redirect(url_for('shopping_list.index'))
+
+
+@shopping_list.route('/<shopping_id>/items/new', defaults={'edit': False}, methods=['GET', 'POST'])
+@shopping_list.route('/<shopping_id>/items/new/<edit>', methods=['GET', 'POST'])
+def items_new(shopping_id, edit):
     form = ItemForm()
     shopping_entry = Shopping.query.get_or_404(int(shopping_id))
     if form.validate_on_submit():
@@ -48,5 +92,39 @@ def items_new(shopping_id):
         shopping_entry.items.append(item)
         db.session.add(shopping_entry)
         db.session.commit()
+    if edit:
+        return redirect(url_for('shopping_list.edit', shopping_id=shopping_id))
+    return render_template('shopping_list/new_items.html', shopping=shopping_entry, form=form)
 
-    return render_template('shopping_list/new_items.html', shopping_entry=shopping_entry, form=form)
+
+@shopping_list.route('<int:shopping_id>/items/edit/<int:item_id>', methods=['GET', 'POST'])
+def edit_item(shopping_id, item_id):
+    form = ItemForm()
+    print(shopping_id)
+    item = Items.query.get_or_404(int(item_id))
+    if form.validate_on_submit():
+        try:
+            item.name = form.name.data
+            item.price = form.price.data
+            item.amount = form.amount.data
+            db.session.commit()
+            flash("Successfully edited item.", "alert alert-info")
+            return redirect(url_for('shopping_list.edit', shopping_id=shopping_id))
+        except DBAPIError as e:
+            flash(str(e), "alert alert-danger")
+
+    return render_template('shopping_list/edit_item.html', item=item, form=form, shopping_id=shopping_id)
+
+
+@shopping_list.route('<int:shopping_id>/items/delete/<int:item_id>')
+def delete_item(shopping_id, item_id):
+    item = Items.query.get_or_404(item_id)
+    try:
+        db.session.delete(item)
+        db.session.commit()
+        flash("Item deleted successfully.", "alert alert-info")
+    except DBAPIError as e:
+        flash(str(e), "alert alert-danger")
+        db.session.rollback()
+
+    return redirect(url_for('shopping_list.edit', shopping_id=shopping_id))

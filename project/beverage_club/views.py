3b592ce9ec677086da4b498c6c36fdbc3ca41376
Thyge Steffensen <s175176@student.dtu.dk>
2018-05-25 19:47:34 +0200

    Layout draft complete. Modal does not contain contents atm. Backend not made. Models not made

--- project/beverage_club/views.py
+++ project/beverage_club/views.py
@@ -5,4 +5,6 @@ from flask import render_template
 
 @beverage_club.route('/')
 def index():
+    # Get the three most sold beers.
+    # If no beers are sold, return the latest beers
     return render_template('beverage_club/index.html')

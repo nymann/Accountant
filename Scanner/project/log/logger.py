36ba2813ce0c491186c382acaabf61ac2b27027e
thygesteffensen <s175176@student.dtu.dk>
2018-05-29 15:33:57 +0200

    Logging enabled

--- Scanner/project/log/logger.py
+++ Scanner/project/log/logger.py
@@ -1,7 +1,11 @@
-# imports
+import time
 
 
-# functions
+def logger_rest(action, http_code, http_url):
+    filename = time.strftime("%Y_%m.log")
+    file = open(filename, 'a')
+    file_text = "{0}\t{1} {2}\t{3}\n".format(action, http_code, http_url, time.strftime("%Y.%m.%d-%H:%M:%S"))
+    print(file_text)
+    file.write(file_text)
+    file.close()
 
-def logger():
-    print("test")
--- Scanner/project/scanner/program.py
+++ Scanner/project/scanner/program.py
@@ -9,8 +9,6 @@ while (1):
     print("Enter beverage id")
     beverage_id = input("> ")
 
-
     payload = {'user_id': user_id, 'beverage_id': beverage_id}
     response = requests.post('http://127.0.0.1:5000/api/buy_beverage', params=payload)
-    print(response.url)
-    print(response.text)
+    logger.logger_rest("Buy Beverage - Scanner", response, response.url)

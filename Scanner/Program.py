0ee81ab772fd2fff7f8a0dcafab6d9c46cfce0c5
Thyge Steffensen <s175176@student.dtu.dk>
2018-05-29 14:30:34 +0200

    Scanner: Program initialized

---
+++ Scanner/Program.py
@@ -0,0 +1,15 @@
+import requests
+
+print("This program is still in developments phase")
+
+while(1):
+
+    print("Enter a user id")
+    user_id = input("> ")
+    print("Enter beverage id")
+    beverage_id = input("> ")
+
+    payload = {'user_id': user_id, 'beverage_id': beverage_id}
+    response = requests.post('http://127.0.0.1:5000/api/buy_beverage', params=payload)
+    print(response.url)
+    print(response.text)

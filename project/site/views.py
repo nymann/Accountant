ed5c3f4c9931f36b44fa36ae9f35d090a0034ce4
Kristian Nymann Jakobsen <nymannjakobsen@gmail.com>
2018-06-02 17:18:00 +0200

    only active users.

--- project/site/views.py
+++ project/site/views.py
@@ -67,17 +67,18 @@ def profile(user_id):
     ).all()
 
     shopping_expenses = 0.0
-    for shopping in non_accounted_shopping_entries:
-        active_members = db.session.query(
-            func.count(User.id)
-        ).filter(
-            User.active,
-            or_(User.move_in_date.is_(None), User.move_in_date <= shopping.date)
-        ).scalar()
-        for item in shopping.items:
-            if (user.move_in_date is None or user.move_in_date <= shopping.date) and (
-                    user.move_out_date is None or user.move_in_date >= shopping.date):
-                shopping_expenses += (item.price * item.amount) / active_members
+    if user.active:
+        for shopping in non_accounted_shopping_entries:
+            active_members = db.session.query(
+                func.count(User.id)
+            ).filter(
+                User.active,
+                or_(User.move_in_date.is_(None), User.move_in_date <= shopping.date)
+            ).scalar()
+            for item in shopping.items:
+                if (user.move_in_date is None or user.move_in_date <= shopping.date) and (
+                        user.move_out_date is None or user.move_in_date >= shopping.date):
+                    shopping_expenses += (item.price * item.amount) / active_members
 
     shopping_income = db.session.query(
         func.sum(Items.price * Items.amount)
@@ -99,19 +100,20 @@ def profile(user_id):
         Dinner.accounted.is_(False)
     ).all()
     dinner_expenses = 0.0
-    for dinner in non_accounted_dinners:
-        if user not in dinner.participants:
-            continue
-        # How many participated?
-        number_of_guests = 0
-        for guest in dinner.guests:
-            number_of_guests += guest.number_of_guests
-        number_of_participants = len(dinner.participants) + number_of_guests
-        dinner_expenses += dinner.price / number_of_participants
-        for guest in dinner.guests:
-            if guest.user_id is user_id:
-                # It's our guest.
-                dinner_expenses += guest.number_of_guests * dinner.price / number_of_participants
+    if user.active:
+        for dinner in non_accounted_dinners:
+            if user not in dinner.participants:
+                continue
+            # How many participated?
+            number_of_guests = 0
+            for guest in dinner.guests:
+                number_of_guests += guest.number_of_guests
+            number_of_participants = len(dinner.participants) + number_of_guests
+            dinner_expenses += dinner.price / number_of_participants
+            for guest in dinner.guests:
+                if guest.user_id is user_id:
+                    # It's our guest.
+                    dinner_expenses += guest.number_of_guests * dinner.price / number_of_participants
 
     form = UserForm()
     if form.validate_on_submit() and (current_user.id is user.id or current_user.admin):

3cedf8083707ee740766bff95a196439339592ff
Kristian Nymann Jakobsen <nymannjakobsen@gmail.com>
2018-06-13 23:21:38 +0200

    Sentry is confirmed working :)

--- project/__init__.py
+++ project/__init__.py
@@ -131,8 +131,3 @@ def load_user(user_id):
 def logout():
     logout_user()
     return redirect(url_for('site.index'))
-
-
-@app.route('/sentry_test')
-def sentry_test():
-    sentry.captureMessage("BLA BLA")

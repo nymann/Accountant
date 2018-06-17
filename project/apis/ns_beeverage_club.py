c0f32b658860b62f2c806bde38e32ecaa1667774
thygesteffensen <s175176@student.dtu.dk>
2018-06-17 15:39:26 +0200

    Added beverage by id

--- project/apis/ns_beeverage_club.py
+++ project/apis/ns_beeverage_club.py
@@ -1,6 +1,5 @@
-from flask_restplus import Namespace, Resource, fields
+from flask_restplus import Namespace, Resource, fields, reqparse
 from project.models import Beverage
-
 
 # Creating the Namspace with desciption
 api = Namespace('beverage_club', description='Beverage Club related operations')
@@ -11,11 +10,6 @@ beverage = api.model('Beverage', {
     'name': fields.String(required=True, description='The Beverage name'),
 })
 
-# NO!
-CATS = [
-    {'id': 'felix', 'name': 'Felix'},
-]
-
 
 @api.route('/')
 class BeverageList(Resource):
@@ -25,3 +19,16 @@ class BeverageList(Resource):
         '''List all Beverages'''
         return Beverage.query.all()
 
+
+@api.route('/byid')
+class Beverage(Resource):
+    @api.doc('beverages')
+    @api.marshal_list_with(beverage)
+    def post(self):
+        '''Get one Beverage'''
+        parser = reqparse.RequestParser()
+        parser.add_argument('beverage_id', type=int, help='The id of the beverage you want')
+        beverage_id = parser.parse_args()
+        print(beverage_id)
+        print('test')
+        return Beverage.query.get(beverage_id)

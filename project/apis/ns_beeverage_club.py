2a5048117570fb70362623ad6aefeff2e417e57b
thygesteffensen <s175176@student.dtu.dk>
2018-06-17 15:48:25 +0200

    testing

--- project/apis/ns_beeverage_club.py
+++ project/apis/ns_beeverage_club.py
@@ -1,5 +1,6 @@
 from flask_restplus import Namespace, Resource, fields, reqparse
-from project.models import Beverage
+from project.models import Beverage, db
+
 
 # Creating the Namspace with desciption
 api = Namespace('beverage_club', description='Beverage Club related operations')
@@ -18,7 +19,7 @@ class BeverageList(Resource):
     def get(self):
         '''List all Beverages'''
         return Beverage.query.all()
-
+        # return db.session.query(Beverage).query.all()
 
 @api.route('/byid')
 class Beverage(Resource):
@@ -29,6 +30,4 @@ class Beverage(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument('beverage_id', type=int, help='The id of the beverage you want')
         beverage_id = parser.parse_args()
-        print(beverage_id)
-        print('test')
         return Beverage.query.get(beverage_id)

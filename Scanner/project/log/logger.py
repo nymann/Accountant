62b7f98fc5a212b5c4c3ae82ad86a27520e3f046
thygesteffensen <s175176@student.dtu.dk>
2018-08-20 14:41:43 +0200

    Modifications made to API.
    Added error handling and improved logging

--- Scanner/project/log/logger.py
+++ Scanner/project/log/logger.py
@@ -2,10 +2,19 @@ import time
 
 
 def logger_rest(action, http_code, http_url):
+    log_to_file("{0}\t{1} {2}\t{3}\n".format(action, http_code, http_url, time.strftime("%Y.%m.%d-%H:%M:%S")))
+
+
+def logger_error(user_id, beverage_id):
+    log_to_file("{0}\t{1}{2}\t{3}{4}\t{5}\n".format('An error occurred', 'User ID: ', user_id, 'Beverage ID: ',
+                                                    beverage_id, time.strftime("%Y.%m.%d-%H:%M:%S")))
+
+
+def log_to_file(log_msg):
     filename = time.strftime("%Y_%m.log")
     file = open(filename, 'a')
-    file_text = "{0}\t{1} {2}\t{3}\n".format(action, http_code, http_url, time.strftime("%Y.%m.%d-%H:%M:%S"))
-    print(file_text)
-    file.write(file_text)
-    file.close()
 
+    print(log_msg)
+
+    file.write(log_msg)
+    file.close()
